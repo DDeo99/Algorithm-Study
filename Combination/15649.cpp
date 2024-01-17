ad4b9ed7998ca4e2ea7aeb33e1de82d510fddbef
DDeo99 <yoondukwoo@gmail.com>
2024-01-17 19:49:18 +0900

    BOJ 백준 15649번 N과 M (1)

---
+++ Combination/15649.cpp
@@ -0,0 +1,49 @@
+// BOJ 백준 15649번 N과 M (1)
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void BT(vector<int> &nums,vector<int> &flag, int N, int M, int level)
+{
+
+    if(level==M) 
+    {
+        for(int i=0;i<M;i++)
+        {
+            cout<<nums[i]<<" ";
+        }
+        cout<<'\n';
+        return;
+    }
+
+    for(int i=0;i<N;i++)
+    {
+        if(flag[i]==0)
+        {
+            nums[level]=i+1;
+            flag[i]=1;
+
+            BT(nums,flag,N,M,level+1);
+            flag[i]=0;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin>> N >> M;
+
+    vector<int> nums(M);
+    vector<int> flag(N,0);
+
+    BT(nums,flag,N,M,0);
+
+    return 0;
+}
