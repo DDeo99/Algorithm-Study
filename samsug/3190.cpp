ba51f7df570cd3b31588c71a627aae138c20e3f2
DDeo99 <yoondukwoo@gmail.com>
2024-03-19 01:14:24 +0900

    BOJ 백준 3190번 뱀

---
+++ samsug/3190.cpp
@@ -0,0 +1,99 @@
+// BOJ 백준 3190번 뱀
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int N,K,L;
+int t=0;
+vector<vector<int>> map;
+queue<pair<int,int>> snake;
+queue<pair<int,char>> nextD;
+int sx,sy;
+// 하 우 상 좌
+int dy[]={1,0,-1,0};
+int dx[]={0,1,0,-1};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin>>N;
+    cin>>K;
+    map.resize(N,vector<int>(N,0));
+    sx=0;
+    sy=0;
+    //뱀은 1
+    map[sy][sx]=1;
+    snake.push({sy,sx});
+
+    for(int i=0;i<K;i++)
+    {
+        int x,y;
+        cin>>y>>x;
+        // 사과는 2
+        map[y-1][x-1]=2;
+    }
+
+    cin>>L;
+    
+    for(int i=0;i<L;i++)
+    {
+        int X;
+        char C;
+        // X초 후에 방향 왼쪽 L 오른쪽 D으로 회전
+        cin>>X>>C;
+        nextD.push({X,C});
+    }  
+    int idx=1;
+    while(1)
+    {
+        sx+=dx[idx];
+        sy+=dy[idx];
+        t++;
+        if(sx<0 || sx>N-1 || sy<0 || sy>N-1)
+            break;
+        if(map[sy][sx]==1)
+            break;
+        else if(map[sy][sx]==2)
+        {
+            map[sy][sx]=1;
+            snake.push({sy,sx});
+        }
+        else if(map[sy][sx]==0)
+        {
+            map[snake.front().first][snake.front().second]=0;
+            snake.pop();
+            map[sy][sx]=1;
+            snake.push({sy,sx});
+        }
+
+        
+
+        if(!nextD.empty())
+        {
+            if(t==nextD.front().first)
+            {
+                if(nextD.front().second=='D')
+                {
+                    idx=(idx+3)%4;
+                    nextD.pop();
+                }
+
+                else if(nextD.front().second=='L')
+                {
+                    idx=(idx+1)%4;
+                    nextD.pop();
+                }
+                
+            }
+        }
+    }
+    cout<<t;
+
+
+    return 0;
+}
