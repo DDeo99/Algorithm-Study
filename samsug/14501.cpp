b3bda4c2514d0fc4d9fae8b34310c45a2379ac2b
DDeo99 <yoondukwoo@gmail.com>
2024-04-01 09:30:07 +0900

    BOJ 백준 14501번 퇴사

--- samsug/14501.cpp
+++ samsug/14501.cpp
@@ -14,7 +14,7 @@ int N;
 vector<pair<int,int>> dnr;
 // 그 날에
 vector<int> cache;
-
+/*
 int main()
 {
     ios::sync_with_stdio(false);
@@ -44,5 +44,43 @@ int main()
     }
 
     cout<<maxCost;
+    return 0;
+}
+*/
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin>>N;
+    // N+1날 퇴사
+    // N일까지 상담 날짜 받음
+    cache.resize(N+2,0);
+    dnr.resize(N+1);
+
+    for(int i=1;i<=N;i++)
+    {
+        int T,P;
+        cin>>T>>P;
+        dnr[i].first=T;
+        dnr[i].second=P;
+    }
+    int maxCost=0;
+    for(int i=1;i<=N;i++)
+    {
+        cache[i]=max(cache[i],maxCost);
+
+        int nextDay=i+dnr[i].first;
+
+        if(nextDay<=N+1)
+        {
+            cache[nextDay]=max(cache[i]+dnr[i].second,cache[nextDay]);
+        }
+        maxCost=max(cache[i],maxCost);
+    }
+    sort(cache.begin(),cache.end());
+    cout<<cache[N+1];
+
     return 0;
 }
