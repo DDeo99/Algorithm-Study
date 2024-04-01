5d5e7b5928dbf781215b05b2fca4f131fd1f8358
DDeo99 <yoondukwoo@gmail.com>
2024-04-01 10:09:58 +0900

    BOJ 백준 14502번 연구소

--- samsug/14502.cpp
+++ samsug/14502.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int N,M;
 vector<vector<int>> map;
 vector<vector<int>> temp;
-//vector<vector<bool>> visited;
+vector<vector<bool>> visited;
 vector<bool> visited2;
 vector<pair<int,int>> mapArray;
 //queue<pair<int,int>> q;
@@ -18,7 +18,121 @@ int ans=0;
 int dy[]={-1,1,0,0};
 int dx[]={0,0,-1,1};
 
+void bfs()
+{
+    queue<pair<int,int>> q;
 
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=1;j<=M;j++)
+        {
+            if(temp[i][j]==2)
+            {
+                q.push({i,j});
+            }
+        }
+    }
+
+    while(!q.empty())
+    {
+        pair cur=q.front();
+        int cy=cur.first;
+        int cx=cur.second;
+        q.pop();
+
+        for(int i=0;i<4;i++)
+        {
+            int ny=cy+dy[i];
+            int nx=cx+dx[i];
+
+            if(ny<1||ny>N||nx<1||nx>M)
+                continue;
+            if(temp[ny][nx]==0)
+            {
+                temp[ny][nx]=2;
+                q.push({ny,nx});
+            }
+        }
+    }
+}
+
+void dfs(int level,int idx)
+{
+    if(level==3)
+    {
+        temp=map;
+
+        int cnt=0;
+        for(int n=0;n<mapArray.size();n++)
+        {
+            if(cnt==3)
+                break;
+            if(visited2[n]==1)
+            {
+                int y=mapArray[n].first;
+                int x=mapArray[n].second;
+                temp[y][x]=1;
+                cnt++;
+            }
+        }
+
+        bfs();
+
+        int res=0;
+        for(int i=1;i<=N;i++)
+        {
+            for(int j=1;j<=M;j++)
+            {
+                if(temp[i][j]==0)
+                    res++;
+            }
+        }
+        ans=max(res,ans);
+        return;
+    }
+
+    for(int i=idx;i<mapArray.size();i++)
+    {
+        if(visited2[i]==1)
+            continue;
+        visited2[i]=1;
+        dfs(level+1,i);
+        visited2[i]=0;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin>>N>>M;
+
+    map.resize(N+1,vector<int> (M+1,0));
+    temp.resize(N+1,vector<int> (M+1,0));
+    visited.resize(N+1,vector<bool> (M+1,0));
+
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=1;j<=M;j++)
+        {
+            cin>>map[i][j];
+            if(map[i][j]==0)
+            {
+                mapArray.push_back({i,j});
+            }
+        }
+    }
+    visited2.resize(mapArray.size(),0);
+
+    dfs(0,0);
+    
+    cout<<ans;
+
+    return 0;
+}
+
+/*
 void bfs()
 {    
     queue<pair<int, int>> q;
@@ -138,3 +252,4 @@ int main()
 
     return 0;
 }
+*/
